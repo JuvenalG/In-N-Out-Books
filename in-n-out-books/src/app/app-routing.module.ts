d55d47a357dda5f6ec161721dd062951bc9f73fb
Juvenal Gonzalez <codingforwebdevelopment@gmail.com>
2021-02-23 00:43:02 -0800

    c

--- in-n-out-books/src/app/app-routing.module.ts
+++ in-n-out-books/src/app/app-routing.module.ts
@@ -37,7 +37,7 @@ const routes: Routes = [
     path: "wishlist" ,
     component: WishlistComponent
   },
-  { path: '**',
+  { path: '**',         //used to redirect improper paths back to booklist
     component:  BookListComponent }
 ];
 
